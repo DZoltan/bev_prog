79bd7df0606e2b7e8f43c2d3d301e3e2819ba518
DZoltan <55488265+DZoltan@users.noreply.github.com>
2019-09-20 12:57:15 +0200

    Add files via upload

---
+++ drill_3.cpp
@@ -0,0 +1,46 @@
+#include "std_lib_facilities.h"
+
+int main()
+
+{
+
+string first_name;
+string friend_name;
+int age = 0;
+char friend_sex = 'f' ;
+//string simple_error = "Viccelsz! \n";
+
+cout << "Kinek szeretnél írni? \n";
+cin >> first_name ;
+
+cout << "Életkora: \n";
+cin >> age;
+if(age < 0 || age > 110) 
+	simple_error( " you ' re kidding! ");
+
+
+cout << "Ki a másik barátod?\n" ;
+cin >> friend_name;
+cout << "Mi ennek a barátodnak a neme? (kérlek f (férfi) -fel vagy n (nő)-nel válaszolj)\n" ;
+cin >> friend_sex;
+
+cout << "Kedves " << first_name << ", \n" << "Rég találkoztunk. Hogy vagy? \n Láttad mostanában "
+	<< friend_name << "-t? \n";
+if (friend_sex == 'f')
+	cout << "Monnd meg neki kérlek (ffi) , hogy hívjon fel!\n" ;
+else if (friend_sex == 'n')
+	cout << "Monnd meg neki kérlek (nő) , hogy hívjon fel!\n" ; 
+
+cout << "Hallottam most volt a " << age << " születésnapod! Isten éltessen sokáig\n " ;
+
+if ( age < 12 )
+	cout << "Jövőre már " << age + 1 << " leszel ! \n" ;
+else if (age == 17)
+	cout << "Jövőre már szavazhatsz! \n" ;
+else if (age > 70)
+	cout << "Remélem élvezed a nyugdíjadat! \n";
+
+
+cout << "Üdvözlettel : \n \n" ;
+
+}
